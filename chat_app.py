017f387db9b336b50b4b88ac605b5014ae9045e7
Rajesh G <rajgoaj@gmail.com>
2024-05-10 17:58:23 -0500

    message.db: New component chat_app.py: use sqlite3 db to retain chat history

--- chat_app.py
+++ chat_app.py
@@ -1,8 +1,18 @@
 import streamlit as st
 import requests
 import json
+import sqlite3
 
 chat_history = []
+
+conn = sqlite3.connect('message.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS messages (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+    message TEXT NOT NULL
+)''') 
+
+
 
 
 def call_web_service(message):
@@ -18,8 +28,16 @@ if st.button("Send"):
         chat_history.append("You :" + " " + user_input)
         chat_history.append("DocChat :" + " " + response["message"])
         for message in chat_history:
-            st.write(message)
+            format_sql_insert = "INSERT INTO messages (message) VALUES ('{value}')".format(value=message)
+            print("DEBUG:  " + format_sql_insert)
+            c.execute(format_sql_insert)
+        conn.commit()
 
+        c.execute("Select message from messages order by id desc")
+        msg_from_db = c.fetchall()
+
+        for op_message in msg_from_db:
+            st.write(op_message[0])
 
 if 'button_clicked' not in st.session_state:
     st.session_state.button_clicked = False
